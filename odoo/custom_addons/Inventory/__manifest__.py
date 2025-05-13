2688dac89c1d185cca9fa30f9c73ca06cb10bd5c
Pradhap1802 <pradhap@processdrive.com>
2025-05-13 23:19:57 +0530

    Add files via upload

---
+++ odoo/custom_addons/Inventory/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': ' Warehouse',
+    'version': '1.0',
+    'depends': ['base','stock'],
+    'data': [
+        'views/inventory.xml'
+    ],
+    'installable': True,
+    'application':True
+}
---
+++ odoo/custom_addons/Manufacturing/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Orders',
+    'version': '1.0',
+    'depends': ['base','mrp'],
+    'data': [
+        'views/mrp.xml'
+    ],
+    'installable': True,
+    'application':True
+}
